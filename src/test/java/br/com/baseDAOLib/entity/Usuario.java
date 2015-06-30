a047a4792b7af9eabf404e0ddb7728295e3d35c0
basedaolib <basedaolib@gmail.com>
2015-06-30 14:08:04 -0300

    Update Usuario.java

--- src/test/java/br/com/baseDAOLib/entity/Usuario.java
+++ src/test/java/br/com/baseDAOLib/entity/Usuario.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.PrePersist;
 @Entity
 //flavio
-//mmm543132fsdfsdfsd
+//mmm543132
 public class Usuario extends EntityId{
 
 	private static final long serialVersionUID = -3327573295169448466L;
